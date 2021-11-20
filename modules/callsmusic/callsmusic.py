d2e04f5d06d182fcf886c2a0bb817fa550e3c968
Attitude king <93428427+ItsAttitudeking@users.noreply.github.com>
2021-11-20 15:06:39 +0530

    Create callsmusic.py

---
+++ modules/callsmusic/callsmusic.py
@@ -0,0 +1,29 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import InputAudioStream
+
+from modules import config
+from . import queues
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+pytgcalls = PyTgCalls(client)
+
+
+@pytgcalls.on_stream_end()
+async def on_stream_end(client: PyTgCalls, update: Update) -> None:
+    chat_id = update.chat_id
+    queues.task_done(chat_id)
+
+    if queues.is_empty(chat_id):
+        await pytgcalls.leave_group_call(chat_id)
+    else:
+        await pytgcalls.change_stream(
+            chat_id, 
+            InputAudioStream(
+                queues.get(chat_id)["file"],
+            ),
+        )
+
+
+run = pytgcalls.start
